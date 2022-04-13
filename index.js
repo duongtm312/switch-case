003182e714415f5302c8d3d3af4f3988ce5c830b
duongtm312 <103014462+duongtm312@users.noreply.github.com>
2022-04-13 10:00:29 +0700

    up

---
+++ index.js
@@ -0,0 +1,30 @@
+
+   a = prompt('a?', '');
+    switch (a) {
+    case 0:
+    alert('0');
+    break;
+    case 1:
+    alert(1);
+    break;
+    case 2:
+    case 3:
+    alert('2,3');
+    break;
+}
+   switch (browser) {
+       case 'Edge':
+           alert("You've got the Edge!");
+           break;
+       case 'Chrome':
+       case 'Firefox':
+       case 'Safari':
+       case 'Opera':
+           alert('Okay we support these browsers too');
+           break;
+       default:
+           alert('We hope that this page looks ok!');
+   }
+
+
+
